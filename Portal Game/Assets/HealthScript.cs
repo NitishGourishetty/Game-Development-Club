bafda3a999421f8dc18fbd6716abf4c87021e335
Surya Jasper <45545431+suryajasper@users.noreply.github.com>
2021-02-19 14:26:39 -0800

    updated project files

---
+++ Portal Game/Assets/HealthScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthScript : MonoBehaviour
+{
+    public Slider healthSlider;
+    public TextMeshProUGUI healthHp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthSlider.value = 0.5f;
+        healthHp.text = "50";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
